61241fa88822deedd8d6e589c3c435bacd7de8e5
drgn <62900543+dnrecording@users.noreply.github.com>
2021-10-15 20:30:30 +0700

    builder pattern

---
+++ SoftArchDesign/src/main/java/creational/JSONBookMetadataFormatter.java
@@ -0,0 +1,50 @@
+package creational;
+
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JSONBookMetadataFormatter implements BookMetadataFormatter {
+
+    JSONObject book_obj = new JSONObject();
+    JSONArray book_array = new JSONArray();
+
+    public JSONBookMetadataFormatter() throws IOException {
+        reset();
+    }
+
+    @Override
+    public BookMetadataFormatter reset() {
+        book_obj = new JSONObject();
+        book_array = new JSONArray();
+        book_obj.put("Books", book_array);
+        // Please implement this method. You may create additional methods as you see fit.
+        return null;
+    }
+
+    @Override
+    public BookMetadataFormatter append(Book b) {
+        var bo = new JSONObject();
+        bo.put("ISBN", b.getISBN());
+        bo.put("Title", b.getTitle());
+        bo.put("Publisher", b.getPublisher());
+
+        var ba = new JSONArray();
+        for(var i : b.getAuthors()){
+            ba.add(i);
+        }
+        bo.put("Authors", ba);
+
+        book_array.add(bo);
+        
+        // Please implement this method. You may create additional methods as you see fit.
+        return this;
+    }
+
+    @Override
+    public String getMetadataString() {
+        // Please implement this method. You may create additional methods as you see fit.
+        return book_obj.toString();
+    }
+}
